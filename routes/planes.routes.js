0c680daa878e5ae4252576253eb5f08963ea3234
Isas321 <javier.isas321@gmail.com>
2024-06-04 11:17:13 -0300

    se añade obtener PlanesHabilitados y CrearPlan, probado en potman

--- routes/planes.routes.js
+++ routes/planes.routes.js
@@ -2,13 +2,19 @@ const express = require("express");
 const {
   consultarPlanes,
   actualizarPlan,
+  CargarPlan,
+  CambioEstadoPlan,
+  ObtenerPlanesHabilitados,
 } = require("../controllers/planes.controllers");
 const auth = require("../middleware/auth");
 const { check } = require("express-validator");
 const multer = require("../middleware/multer");
 const router = express.Router();
 
+router.get("/planesHabilitados", ObtenerPlanesHabilitados);
 router.get("/", consultarPlanes);
+router.post("/", CargarPlan);
+router.put("/cambioEstadoPlan/:id", CambioEstadoPlan);
 router.put("/:id", actualizarPlan);
 
 module.exports = router;
