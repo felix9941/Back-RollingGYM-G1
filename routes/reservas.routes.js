1bef29eea83633f9bbcebf6549cbc2bbd7426f96
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-06-04 13:01:20 -0300

    se crea el endpoint drearReserva que sirve para agregar una claase a las reservas de un usuario.

--- routes/reservas.routes.js
+++ routes/reservas.routes.js
@@ -1,3 +1,7 @@
 const express = require("express");
+const { crearReserva } = require("../controllers/reservas.controllers");
 const router = express.Router();
+
+router.post("/", crearReserva);
+
 module.exports = router;
