5ac6b100484520dcd30a7f64d5b0983dfe7a6436
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-06-04 13:44:38 -0300

    se crea el endpoint para eliminar una clase de las reservas de un cliente

--- controllers/reservas.controllers.js
+++ controllers/reservas.controllers.js
@@ -1,4 +1,5 @@
 const ReservasModel = require("../models/reservasSchema");
+const mongoose = require("mongoose");
 
 const crearReserva = async (req, res) => {
   try {
@@ -12,10 +13,10 @@ const crearReserva = async (req, res) => {
     }
     const reserva = await ReservasModel.findById(idReserva);
     if (reserva.clases.includes(idClase)) {
-      return res.status(400).send("La clase ya se encuentra en la reserva");
+      return res.status(400).send("La clase ya se encuentra en las reservas");
     }
     const agregarClase = await ReservasModel.findByIdAndUpdate(
-      { id: idReserva },
+      { _id: idReserva },
       { $push: { clases: idClase } },
       { new: true }
     );
@@ -29,6 +30,38 @@ const crearReserva = async (req, res) => {
   }
 };
 
+const eliminarReserva = async (req, res) => {
+  try {
+    const idReserva = req.params.id;
+    const idClase = req.body.idClase;
+    if (!mongoose.Types.ObjectId.isValid(idReserva)) {
+      return res.status(400).send("Id Reserva inválido");
+    }
+    if (!mongoose.Types.ObjectId.isValid(idClase)) {
+      return res.status(400).send("Id Clase inválido");
+    }
+    const reserva = await ReservasModel.findById(idReserva);
+    if (!reserva.clases.includes(idClase)) {
+      return res.status(400).send("La clase no se encuentra en las reservas");
+    }
+    const quitarClase = await ReservasModel.findByIdAndUpdate(
+      { _id: idReserva },
+      { $pull: { clases: idClase } },
+      { new: true }
+    );
+    if (!quitarClase) {
+      return res.status(400).json({ message: "No se pudo crear la reserva" });
+    }
+    res
+      .status(200)
+      .json({ message: "Reserva eliminada con éxito", quitarClase });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "No se pudo eliminar la reserva" });
+  }
+};
+
 module.exports = {
   crearReserva,
+  eliminarReserva,
 };
