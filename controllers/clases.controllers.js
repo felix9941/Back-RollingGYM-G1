522e8b85e4d7fa896068cf794a68bae6392ade7e
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-06-03 22:32:17 -0300

    modificacion en el manejo de la hora en la creacion de clases

--- controllers/clases.controllers.js
+++ controllers/clases.controllers.js
@@ -1,19 +1,9 @@
 const ClasesModel = require("../models/clasesSchema");
 const { validationResult } = require("express-validator");
-const moment = require("moment");
 
 const crearClase = async (req, res) => {
   try {
-    const { nombre, dia, hora, categoria, idProfesor, cupo } = req.body;
-    const soloHora = moment(hora, "HH:mm").toDate();
-    const newClase = new ClasesModel({
-      nombre,
-      dia,
-      hora: soloHora,
-      categoria,
-      idProfesor,
-      cupo,
-    });
+    const newClase = new ClasesModel(req.body);
     await newClase.save();
     res.status(200).json({ message: "Clase creada con exito", newClase });
   } catch (error) {
