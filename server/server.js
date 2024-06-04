e459574ddaf057a2b52bf16f965846bcca3d324d
Isas321 <javier.isas321@gmail.com>
2024-06-04 02:23:03 -0300

    creacion de los archivos necesarios para categorias + ruta

--- server/server.js
+++ server/server.js
@@ -23,6 +23,8 @@ class Servidor {
     this.app.use("/api/profesores", require("../routes/profesores.routes"));
     this.app.use("/api/reservas", require("../routes/reservas.routes"));
     this.app.use("/api/clases", require("../routes/clases.routes"));
+    this.app.use("/api/reservas", require("../routes/reservas.routes"));
+    this.app.use("/api/categorias", require("../routes/categorias.routes"));
   }
 
   listen() {
