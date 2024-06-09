1cc735bd21dc0fd8ee6bcee87606b5fa708c9263
Isas321 <javier.isas321@gmail.com>
2024-06-09 19:48:43 -0300

    Limitacion en el nombre de producto

--- models/productosSchema.js
+++ models/productosSchema.js
@@ -4,9 +4,12 @@ const ProductosSchema = new Schema({
   nombre: {
     type: String,
     required: true,
+    maxlength: [50, "El nombre no puede tener más de 50 caracteres"],
+    minlength: [2, "El nombre no puede tener menos de 2 caracteres"],
   },
   foto: {
     type: String,
+    required: true,
   },
   deleted: {
     type: Boolean,
