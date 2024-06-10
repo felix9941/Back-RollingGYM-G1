33ff22c939e4845600b44a869ece70faa166f91a
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-06-10 18:50:55 -0300

    Se agrega el manejo de imagen con cloudinary en la creacion de categorias, se crea el endpoint para filtrar categorias por planes

--- routes/categorias.routes.js
+++ routes/categorias.routes.js
@@ -6,17 +6,19 @@ const {
   ActualizarCategoria,
   CambioEstadoCategoria,
   EliminarCatFisicamente,
+  ObtenerCategoriasPorPlan,
 } = require("../controllers/categorias.controllers");
 const auth = require("../middleware/auth");
 const { check } = require("express-validator");
 const multer = require("../middleware/multer");
 const router = express.Router();
 
-router.get("/categoriasHabilitadas", ObtenerCategoriasHabilitadas);
-router.get("/", ConsultarCategorias);
-router.post("/", CargarCategoria);
-router.put("/:id", ActualizarCategoria);
 router.put("/cambioEstadoCategoria/:id", CambioEstadoCategoria);
+router.get("/categoriasHabilitadas", ObtenerCategoriasHabilitadas);
+router.get("/categoriasPlan", ObtenerCategoriasPorPlan);
+router.put("/:id", ActualizarCategoria);
 router.delete("/:id", EliminarCatFisicamente);
+router.get("/", ConsultarCategorias);
+router.post("/", multer.single("foto"), CargarCategoria);
 
 module.exports = router;
