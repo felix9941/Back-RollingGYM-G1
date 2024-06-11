a5baeab2d5244bbeb8fc0291800d7965159d6298
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-06-11 10:05:23 -0300

    se modifica el controller ObtenerCategoriasPorPlan para que use el plan que guardamos en el req en el middleware de obtenerPlan, se modifican los filtros para determinar que categorias traer segun el plan y se importa y usa obtener plan en routes

--- routes/categorias.routes.js
+++ routes/categorias.routes.js
@@ -12,10 +12,11 @@ const auth = require("../middleware/auth");
 const { check } = require("express-validator");
 const multer = require("../middleware/multer");
 const router = express.Router();
+const obtenerPlan = require("../middleware/obtenerPlan");
 
 router.put("/cambioEstadoCategoria/:id", CambioEstadoCategoria);
 router.get("/categoriasHabilitadas", ObtenerCategoriasHabilitadas);
-router.get("/categoriasPlan", ObtenerCategoriasPorPlan);
+router.get("/categoriasPlan", obtenerPlan(), ObtenerCategoriasPorPlan);
 router.put("/:id", ActualizarCategoria);
 router.delete("/:id", EliminarCatFisicamente);
 router.get("/", ConsultarCategorias);
