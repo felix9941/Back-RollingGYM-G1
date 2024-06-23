5750de8a03f71f278a7f47acdacca670d37b9d52
Isas321 <javier.isas321@gmail.com>
2024-06-23 17:13:15 -0300

    Generalizacion para que obtenerDatos sea para clientes, profes y daministradores

--- middleware/obtenerDatos.js
+++ middleware/obtenerDatos.js
@@ -7,7 +7,19 @@ module.exports = () => async (req, res, next) => {
       return res.status(400).json({ message: "Error al encontrar el token" });
     }
     const verifyToken = jwt.verify(token, process.env.SECRET_KEY_JWT);
-    req.id = await verifyToken.administrador.id;
+    const roles = ["administrador", "cliente", "profesor"];
+    for (const role of roles) {
+      if (verifyToken[role]?.id) {
+        req.id = verifyToken[role].id;
+        req.role = role;
+        break;
+      }
+    }
+    if (!req.id) {
+      return res
+        .status(400)
+        .json({ message: "Error: Token inválido, no se encontró ID" });
+    }
     next();
   } catch (error) {
     console.log(error);
