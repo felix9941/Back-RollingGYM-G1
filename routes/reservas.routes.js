83f2c8d61b1b375694d16dab8c1f7502472eaf2c
Felix Figueroa <felixfigueroa41@gmail.com>
2024-06-29 00:49:38 -0300

    Se hacen modificaciones en el endopoint de eliminarReservas de Reservas tanto en su controller como routes

--- routes/reservas.routes.js
+++ routes/reservas.routes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const {
   crearReserva,
   eliminarReserva,
+  obtenerReservasPorCliente,
 } = require("../controllers/reservas.controllers");
 const auth = require("../middleware/auth");
 const obtenerReservas = require("../middleware/obtenerReservas");
 const router = express.Router();
 
 router.post("/:idClase", obtenerReservas(), crearReserva);
-router.delete("/:id", eliminarReserva);
-
+router.delete("/eliminar", obtenerReservas(), eliminarReserva);
+router.get("/", obtenerReservas(), obtenerReservasPorCliente);
 module.exports = router;
