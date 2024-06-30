9ecc779b5b22527b9a7623a602340fefe8e03e79
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-06-30 16:05:46 -0300

    se crean los endpoints para obtener reservas por clientes y para limpiar reservas

--- controllers/reservas.controllers.js
+++ controllers/reservas.controllers.js
@@ -1,4 +1,5 @@
 const ReservasModel = require("../models/reservasSchema");
+const ClasesModel = require("../models/clasesSchema");
 const mongoose = require("mongoose");
 
 const crearReserva = async (req, res) => {
@@ -32,7 +33,7 @@ const crearReserva = async (req, res) => {
 
 const eliminarReserva = async (req, res) => {
   try {
-    const idReserva = req.params.id;
+    const idReserva = req.reservas;
     const idClase = req.body.idClase;
     if (!mongoose.Types.ObjectId.isValid(idReserva)) {
       return res.status(400).send("Id Reserva inválido");
@@ -50,7 +51,9 @@ const eliminarReserva = async (req, res) => {
       { new: true }
     );
     if (!quitarClase) {
-      return res.status(400).json({ message: "No se pudo crear la reserva" });
+      return res
+        .status(400)
+        .json({ message: "No se pudo eliminar la reserva" });
     }
     res
       .status(200)
@@ -61,7 +64,63 @@ const eliminarReserva = async (req, res) => {
   }
 };
 
+const limpiezaReservas = async (req, res) => {
+  try {
+    const idReserva = req.body.reservas;
+    const idClase = req.body.idClase;
+    if (!mongoose.Types.ObjectId.isValid(idReserva)) {
+      return res.status(400).send("Id Reserva inválido");
+    }
+    if (!mongoose.Types.ObjectId.isValid(idClase)) {
+      return res.status(400).send("Id Clase inválido");
+    }
+    const reserva = await ReservasModel.findById(idReserva);
+    if (!reserva.clases.includes(idClase)) {
+      return res.status(200).send("La clase no se encuentra en las reservas");
+    }
+    const quitarClase = await ReservasModel.findByIdAndUpdate(
+      { _id: idReserva },
+      { $pull: { clases: idClase } },
+      { new: true }
+    );
+    if (!quitarClase) {
+      return res
+        .status(400)
+        .json({ message: "No se pudo eliminar la reserva" });
+    }
+    res
+      .status(200)
+      .json({ message: "Reserva eliminada con éxito", quitarClase });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "No se pudo eliminar la reserva" });
+  }
+};
+
+const obtenerReservasPorCliente = async (req, res) => {
+  try {
+    const idReserva = req.reservas;
+    if (!mongoose.Types.ObjectId.isValid(idReserva)) {
+      return res.status(400).send("Id Reserva inválido");
+    }
+
+    const reserva = await ReservasModel.findById(idReserva);
+    if (!reserva) {
+      return res.status(404).send("Reserva no encontrada");
+    }
+
+    const clases = await ClasesModel.find({
+      _id: { $in: reserva.clases },
+    });
+    res.status(200).json({ message: "Reservas obtenidas", clases });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "No se pudo obtener las reservas", error });
+  }
+};
 module.exports = {
   crearReserva,
   eliminarReserva,
+  obtenerReservasPorCliente,
+  limpiezaReservas,
 };
